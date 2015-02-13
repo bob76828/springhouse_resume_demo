f23b939df6442a697d2aa0bef8a2d14c39dd779f
Bob Lin <bob76828@gmail.com>
2015-02-13 21:31:00 +0800

    ADD:books

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+var app = angular.module('BookApp', ['ngMaterial']).config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('blue')
+        .backgroundPalette('blue');
+});;
 
+app.controller('AppCtrl', ['$scope', function($scope){
+    var tabs = [
+        { title: '文章'},
+        { title: '書籍'},
+    ];
+    $scope.tabs = tabs;
+    $scope.selectedIndex = 0;
+}]);
+
+angular.element(document).ready(function () {
+    angular.bootstrap(document, ['BookApp']);
+});
